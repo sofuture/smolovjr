59b374ea8409c731b9e6281c8ba512d4f4aa2659
Jeff Zellner <jeffz@eml.cc>
2016-09-18 15:50:13 -0600

    add worksheet column

--- program.go
+++ program.go
@@ -28,13 +28,17 @@ func (p Program) Print(liftName string, max int, weeks int, increment int) {
 		fmt.Println("---------")
 
 		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight)
-		fmt.Fprintln(w, "\tSets\tReps\t Weight\t")
+		fmt.Fprintln(w, "\tSets\tReps\t Weight\tWorksheet\t")
 
 		for _, day := range p.Days {
 			fmt.Fprintf(w, "%s\t", day.Name)
 			fmt.Fprintf(w, "%d\t", day.Sets)
 			fmt.Fprintf(w, "%d\t", day.Reps)
-			fmt.Fprintf(w, "%d\t\n", int(day.Percentage*float64(max))+(increment*i))
+			fmt.Fprintf(w, "%d\t  ", int(day.Percentage*float64(max))+(increment*i))
+			for j := 0; j < day.Sets; j++ {
+				fmt.Fprint(w, "| ")
+			}
+			fmt.Fprintf(w, "|\t\n")
 		}
 
 		w.Flush()
